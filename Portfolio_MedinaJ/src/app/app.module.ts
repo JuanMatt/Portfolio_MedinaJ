3e2c0ee7fb928a2332779a8cc7eed1a53e44e104
Juan Medina <juancho6322@gmail.com>
2023-05-03 18:26:08 -0300

    banner

--- Portfolio_MedinaJ/src/app/app.module.ts
+++ Portfolio_MedinaJ/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialesComponent } from './componentes/sociales/sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercademiComponent } from './componentes/acercademi/acercademi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent,
+    AcercademiComponent
   ],
   imports: [
     BrowserModule,
